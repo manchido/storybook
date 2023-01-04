4db784e4992826d62f180d2c35457cb0a24ea73b
Desha <kendesha@techy-we.com>
2023-01-04 00:08:48 -0330

    update

--- app.js
+++ app.js
@@ -51,6 +51,7 @@ app.engine('.hbs', exphbs.engine({ helpers: {
     editIcon,
     select,
 },  defaultLayout: 'main', extname: '.hbs'}))
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', '.hbs')
 
 // Session middleware
