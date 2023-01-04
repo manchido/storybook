83ac578fb1ba3f43ef2ea85da162a35f90b60c70
Desha <kendesha@techy-we.com>
2023-01-03 23:30:31 -0330

    update

--- app.js
+++ app.js
@@ -72,7 +72,7 @@ app.use(function (req, res, next) {
 
 //Static folder
 
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, '/public')))
 
 // Routes
 app.use('/', require('./routes/index'))
